cc7e9b59785693a9ff3b6293a6c0ce1dfa2e1ac4
Curtis Nix <curtis.nix@proton.me>
2024-10-02 00:42:35 -0400

    finished fixing output and removing extra print statements

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ def main():
         for words in file_contents.split():
             counter += 1 
     print(f"--- Begin report of {file} ---")
-    print(f"{counter} words in the file")
+    print(f"{counter} words in the file") 
+    print()
     count_dict = count_characters(file_contents)
-    count_dict.sort(reverse=False, value=sort_on)
+    list_of_count_dict = []
     for i in count_dict:
-        print(f"The '{i}' character was found f{count_dict[i]} times")
+        list_of_count_dict.append({"letter":i, "count":count_dict[i]})
+    list_of_count_dict.sort(reverse=True, key=sort_on)
+    for i in list_of_count_dict:
+        print(f"The '{i["letter"]}' character was found {i["count"]} times")
+        print(i["count"])
+
+
+def sort_on(dict):
+    return dict["count"]
 
 def count_characters(book):
     book = book.lower()
